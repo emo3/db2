d9281c09fe173c7a171017e1c48dd5163fd293ca
Ed Overton <e.overton@gmail.com>
2023-04-22 19:10:32 -0400

    fix many items

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,31 +1,18 @@
 #
-# Cookbook Name:: db2
+# Cookbook:: db2
 # Recipe:: default
 #
-# Copyright 2019, Ed Overton
-#
-# Licensed under the Apache License, Version 2.0 (the 'License');
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an 'AS IS' BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright:: 2019, Ed Overton, Apache 2.0
 
 db2binary_dir = "#{Chef::Config[:file_cache_path]}/DB2binaries"
 binaries = [node['db2']['package-name-1']]
 
-package node['db2']['ubuntu'] if node['platform_family'] == 'debian'
-package node['db2']['rhel']   if node['platform_family'] == 'rhel'
+package node['db2']['ubuntu'] if platform_family?('debian')
+package node['db2']['rhel']   if platform_family?('rhel')
 
 directory db2binary_dir do
   mode '0755'
-  not_if { File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
+  not_if { ::File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
   action :create
 end
 
@@ -36,16 +23,15 @@ binaries.each do |package_name|
     command "scp #{node['db2']['ftploginuser']}@#{node['db2']['binaryhost']}:#{node['db2']['ftppath']}/#{package_name} #{db2binary_dir}"
     cwd db2binary_dir
     only_if { node['db2']['remote_mode'] == 'ftp' }
-    not_if { File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
+    not_if { ::File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
   end
 
   # Download file via http
   remote_file "#{db2binary_dir}/#{package_name}" do
     source "#{node['db2']['binaryhost']}/#{node['db2']['ftppath']}/#{package_name}"
-    not_if { File.exist?("#{db2binary_dir}/#{package_name}") }
     mode '0755'
     only_if { node['db2']['remote_mode'] == 'http' }
-    not_if { File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
+    not_if { ::File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
     backup false
     action :create
   end
@@ -54,8 +40,8 @@ binaries.each do |package_name|
     action :run
     command "unzip -q #{package_name}"
     cwd db2binary_dir
-    not_if { File.exist?("#{db2binary_dir}/db2setup") }
-    not_if { File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
+    not_if { ::File.exist?("#{db2binary_dir}/db2setup") }
+    not_if { ::File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
   end
 end
 
@@ -66,13 +52,13 @@ template "#{db2binary_dir}/#{node['db2']['db2-responsefile']}" do
     install_type: node['db2']['install_type']
   )
   mode '0644'
-  not_if { File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
+  not_if { ::File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
 end
 
 execute 'install-db2' do
   command "#{db2binary_dir}/db2setup -r #{db2binary_dir}/#{node['db2']['db2-responsefile']}"
   cwd db2binary_dir
-  not_if { File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
+  not_if { ::File.exist?("#{node['db2']['db2_install_dir']}/bin/db2") }
   action :run
 end
 
